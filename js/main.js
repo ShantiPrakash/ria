7d2c8b791c6d06500c1ee311cb873529fd5125ee
ShantiPrakash <shantiprakash90@gmail.com>
2024-08-06 19:17:03 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,3 +28,31 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 1000); // Match this duration with the CSS transition duration
     }, 5000); // 5000 milliseconds = 5 seconds
 });
+
+var speed = 25;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+  elt = document.getElementById(id);
+  endNbr = Number(document.getElementById(id).innerHTML);
+  endNbr = 100;
+  incNbrRec(0, endNbr, elt);
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, endNbr, elt) {
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function() {//Delay a bit before calling the function again.
+      incNbrRec(i + 1, endNbr, elt);
+    }, speed);
+  }
+}
+
+/*Function called on button click*/
+function incNbr(){
+  incEltNbr("nbr");
+}
+
+incEltNbr("nbr"); /*Call this funtion with the ID-name for that element to increase the number within*/
